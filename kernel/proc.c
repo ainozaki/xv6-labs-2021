aa66be140a216be48efb7133fc2e1535c3664989
ainozaki <aiai10piano@gmail.com>
2022-03-31 06:36:53 +0000

    lec5: print page table

--- kernel/proc.c
+++ kernel/proc.c
@@ -654,3 +654,4 @@ procdump(void)
     printf("\n");
   }
 }
+
--- kernel/vm.c
+++ kernel/vm.c
@@ -432,3 +432,28 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+static void vmprint_core(pagetable_t pagetable, int depth){
+	for (int i = 0; i < 512; i++){
+		pte_t pte = pagetable[i];
+		if ((pte & PTE_V)){
+			/* this PTE points to a lower-level page table. */
+			uint64 child = PTE2PA(pte);
+			for(int j = 0; j < depth; j++){
+				if (j){
+					printf(" ");
+				}
+				printf("..");
+			}
+			printf("%d: pte %p pa %p\n", i, pte, child);
+			if (depth < 3){
+				vmprint_core((pagetable_t)child, depth + 1);
+			}
+		}
+	}
+}
+
+void vmprint(pagetable_t pagetable){
+	printf("page table %p\n", pagetable);
+	vmprint_core(pagetable, 1);
+}
